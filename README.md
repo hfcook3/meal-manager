b4b11de94ed958c71d25d9aa24eb4807a181fed9
Trey Cook <hfcook3@gmail.com>
2020-01-12 13:24:48 -0500

    Initial title list view

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# mealmanager
+# Meal Manager
 
 A cross-platform mobile app designed to aid with tracking meal selection and grocery needs.
--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,39 @@ class MealManager extends StatelessWidget {
 }
 
 class MealListState extends State<MealList> {
+  final List<String> _recipeTitleList = <String>[
+    'Black Bean Burritos',
+    'Sushi',
+    'Lamb Meatballs'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Meal Manager'),
         ),
-        body: Center(
-          child: Text('Helloooo World'),
-        ));
+        body: _buildRecipes());
+  }
+
+  Widget _buildRecipes() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: _recipeTitleList.length * 2,
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) {
+          return Divider(
+            thickness: 2.0,
+          );
+        } else {
+          return _buildRecipeTile(_recipeTitleList[i ~/ 2]);
+        }
+      },
+    );
+  }
+
+  Widget _buildRecipeTile(String recipeTitle) {
+    return ListTile(title: Center(child: Text(recipeTitle)));
   }
 }
 
