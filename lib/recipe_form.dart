b1da675bb028079e224f9b6536e622bfed9d3bfc
Trey Cook <hfcook3@gmail.com>
2020-01-18 14:35:19 -0500

    Finished fist version of recipe form

--- lib/recipe_form.dart
+++ lib/recipe_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class RecipeFormHome extends StatelessWidget {
   @override
@@ -8,14 +9,7 @@ class RecipeFormHome extends StatelessWidget {
         title: Text('New Recipe'),
         leading: BackButton(),
       ),
-      body: RecipeForm(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => RecipeForm()));
-        },
-      ),
+      body: Container(padding: EdgeInsets.all(18.0), child: RecipeForm()),
     );
   }
 }
@@ -29,14 +23,31 @@ class RecipeForm extends StatefulWidget {
 
 class RecipeFormState extends State<RecipeForm> {
   final _formKey = GlobalKey<FormState>();
+  final _uuid = Uuid();
+
+  List<Widget> _ingredientFieldList = List<Widget>();
+  List<Widget> _stepFieldList = List<Widget>();
+
+  @override
+  void initState() {
+    _ingredientFieldList = [_buildIngredientField(_uuid.v1())];
+    _stepFieldList = [_buildStepField(_uuid.v1())];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
         key: _formKey,
-        child: Column(
+        child: ListView(
           children: <Widget>[
+            Text(
+              'Title',
+              style: TextStyle(fontSize: 20),
+            ),
             TextFormField(
+              decoration:
+                  InputDecoration(hintText: 'Enter the recipe title here'),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter a title.';
@@ -44,7 +55,60 @@ class RecipeFormState extends State<RecipeForm> {
                 return null;
               },
             ),
+            Container(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Ingredients',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  RaisedButton(
+                    child: Icon(Icons.add),
+                    color: Colors.deepPurple,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        _ingredientFieldList
+                            .add(_buildIngredientField(_uuid.v1()));
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            Column(
+              children: _ingredientFieldList,
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Steps',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  RaisedButton(
+                    child: Icon(Icons.add),
+                    color: Colors.deepPurple,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        _stepFieldList.add(_buildStepField(_uuid.v1()));
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            Column(
+              children: _stepFieldList,
+            ),
             RaisedButton(
+                color: Colors.deepPurple,
+                textColor: Colors.white,
                 onPressed: () {
                   if (_formKey.currentState.validate()) {
                     Scaffold.of(context).showSnackBar(
@@ -54,5 +118,67 @@ class RecipeFormState extends State<RecipeForm> {
                 child: Text('Submit'))
           ],
         ));
+  }
+
+  Widget _buildIngredientField(String key) {
+    return Row(
+      key: Key(key),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          child: TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Enter an ingredient for your recipe'),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter an ingredient.';
+              }
+              return null;
+            },
+          ),
+        ),
+        RaisedButton(
+          child: Icon(Icons.remove),
+          color: Colors.red,
+          textColor: Colors.white,
+          onPressed: () {
+            setState(() {
+              _ingredientFieldList.removeWhere((form) => form.key == Key(key));
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildStepField(String key) {
+    return Row(
+      key: Key(key),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          child: TextFormField(
+            decoration:
+                InputDecoration(hintText: 'Enter the next step in your recipe'),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter a step.';
+              }
+              return null;
+            },
+          ),
+        ),
+        RaisedButton(
+          child: Icon(Icons.remove),
+          color: Colors.red,
+          textColor: Colors.white,
+          onPressed: () {
+            setState(() {
+              _ingredientFieldList.removeWhere((form) => form.key == Key(key));
+            });
+          },
+        )
+      ],
+    );
   }
 }
