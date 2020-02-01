eaf0cdebd0ab1455978f2b2f4f89eb733774bf27
Trey Cook <hfcook3@gmail.com>
2020-02-01 09:51:49 -0500

    Added initialization based on existing recipe and update DB call

--- lib/recipe_form.dart
+++ lib/recipe_form.dart
@@ -8,6 +8,10 @@ import 'package:sqflite/sqlite_api.dart';
 import 'package:uuid/uuid.dart';
 
 class RecipeFormHome extends StatelessWidget {
+  final Recipe recipeToEdit;
+
+  RecipeFormHome(this.recipeToEdit);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,15 +19,20 @@ class RecipeFormHome extends StatelessWidget {
         title: Text('New Recipe'),
         leading: BackButton(),
       ),
-      body: Container(padding: EdgeInsets.all(18.0), child: RecipeForm()),
+      body: Container(
+          padding: EdgeInsets.all(18.0), child: RecipeForm(recipeToEdit)),
     );
   }
 }
 
 class RecipeForm extends StatefulWidget {
+  final Recipe recipe;
+
+  RecipeForm(this.recipe);
+
   @override
   RecipeFormState createState() {
-    return RecipeFormState();
+    return RecipeFormState(recipe);
   }
 }
 
@@ -31,18 +40,47 @@ class RecipeFormState extends State<RecipeForm> {
   final _formKey = GlobalKey<FormState>();
   final _uuid = Uuid();
 
+  final ingredientsHint = 'Write a new ingredient';
+  final ingredientsValidationText = 'Please enter an ingredient';
+  final ingredientsListName = 'ingredients';
+
+  final stepsHint = 'Write the next step';
+  final stepsValidationText = 'Please enter a step';
+  final stepsListName = 'steps';
+
   Database database;
   List<Widget> _ingredientFieldList = new List<Widget>();
   List<Widget> _stepFieldList = new List<Widget>();
 
-  Recipe _recipe = new Recipe();
+  Recipe _recipe;
+
+  RecipeFormState(this._recipe);
 
   @override
   void initState() {
     _initDb();
 
-    _recipe.ingredients = new List<String>();
-    _recipe.steps = new List<String>();
+    if (_recipe.ingredients == null) {
+      _recipe.ingredients = new List<String>();
+    } else {
+      _ingredientFieldList = List.generate(_recipe.ingredients.length, (i) {
+        return _buildSubField(_uuid.v1(), ingredientsHint,
+            ingredientsValidationText, ingredientsListName,
+            initialValue: _recipe.ingredients[i]);
+      });
+      _recipe.ingredients = new List<String>();
+    }
+
+    if (_recipe.steps == null) {
+      _recipe.steps = new List<String>();
+    } else {
+      _stepFieldList = List.generate(_recipe.steps.length, (i) {
+        return _buildSubField(
+            _uuid.v1(), stepsHint, stepsValidationText, stepsListName,
+            initialValue: _recipe.steps[i]);
+      });
+      _recipe.steps = new List<String>();
+    }
 
     super.initState();
   }
@@ -65,6 +103,7 @@ class RecipeFormState extends State<RecipeForm> {
               style: TextStyle(fontSize: 20),
             ),
             TextFormField(
+              initialValue: _recipe.title,
               decoration:
                   InputDecoration(hintText: 'Enter the recipe title here'),
               onSaved: (String value) {
@@ -95,9 +134,9 @@ class RecipeFormState extends State<RecipeForm> {
                         String key = _uuid.v1();
                         _ingredientFieldList.add(_buildSubField(
                             key,
-                            'Write a new ingredient',
-                            'Please enter an ingredient',
-                            'ingredients'));
+                            ingredientsHint,
+                            ingredientsValidationText,
+                            ingredientsListName));
                       });
                     },
                   )
@@ -123,11 +162,8 @@ class RecipeFormState extends State<RecipeForm> {
                     onPressed: () {
                       setState(() {
                         String key = _uuid.v1();
-                        _stepFieldList.add(_buildSubField(
-                            key,
-                            'Write the next step',
-                            'Please enter a step',
-                            'steps'));
+                        _stepFieldList.add(_buildSubField(key, stepsHint,
+                            stepsValidationText, stepsListName));
                       });
                     },
                   )
@@ -145,7 +181,11 @@ class RecipeFormState extends State<RecipeForm> {
                     onPressed: () {
                       if (_formKey.currentState.validate()) {
                         _formKey.currentState.save();
-                        _insertNewRecipe(context);
+                        if (_recipe.id == null) {
+                          _insertNewRecipe(context);
+                        } else {
+                          _updateRecipe(context);
+                        }
                       }
                     },
                     child: Text('Submit')))
@@ -154,13 +194,15 @@ class RecipeFormState extends State<RecipeForm> {
   }
 
   Widget _buildSubField(
-      String key, String hint, String validationText, String fieldList) {
+      String key, String hint, String validationText, String fieldList,
+      {String initialValue = ''}) {
     return Row(
       key: Key(key),
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Expanded(
           child: TextFormField(
+            initialValue: initialValue,
             decoration: InputDecoration(hintText: hint),
             onSaved: ((String value) {
               if (fieldList == "ingredients") {
@@ -181,6 +223,7 @@ class RecipeFormState extends State<RecipeForm> {
           child: Icon(Icons.remove),
           color: Colors.red,
           textColor: Colors.white,
+          shape: CircleBorder(side: BorderSide.none),
           onPressed: () {
             setState(() {
               if (fieldList == "ingredients") {
@@ -212,5 +255,28 @@ class RecipeFormState extends State<RecipeForm> {
           [id, _recipe.steps[i]]);
     }
     Navigator.pop(context, id);
+  }
+
+  _updateRecipe(BuildContext context) async {
+    await database.rawUpdate('UPDATE Recipes SET title = ? WHERE id = ?',
+        [_recipe.title, _recipe.id]);
+
+    await database
+        .rawDelete('DELETE FROM Ingredients WHERE recipeKey = ?', [_recipe.id]);
+
+    await database
+        .rawDelete('DELETE FROM Steps WHERE recipeKey = ?', [_recipe.id]);
+
+    for (int i = 0; i < _recipe.ingredients.length; i++) {
+      await database.rawInsert(
+          'INSERT INTO Ingredients(recipeKey, ingredient) VALUES (?, ?)',
+          [_recipe.id, _recipe.ingredients[i]]);
+    }
+    for (int i = 0; i < _recipe.steps.length; i++) {
+      await database.rawInsert(
+          'INSERT INTO Steps(recipeKey, step) VALUES (?, ?)',
+          [_recipe.id, _recipe.steps[i]]);
+    }
+    Navigator.pop(context, _recipe.id);
   }
 }
